dd06f9bc44459e31742ec7fff080a14313e9cadf
cw <fromnowwon@gmail.com>
2023-02-15 14:36:36 +0900

    feat: post page 생성

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,29 @@
+
+import fs from "fs";
+import Markdown from "markdown-to-jsx";
+import matter from "gray-matter";
+
+const getPostContent = (slug: string) => { 
+  const folder = "posts/";
+  const file = `${folder}${slug}.md`;
+  const content = fs.readFileSync(file, "utf-8");
+  const matterResult = matter(content);
+  return matterResult;
+}
+
+export const generateStaticParams = async ( ) => {
+  return [{ slug: "" }]
+}
+
+const PostPage = (props: any) => {
+  const slug = props.params.slug;
+  const post = getPostContent(slug);
+  return (
+    <div>
+      <h1>{post.data.title}</h1>
+      <Markdown>{post.content}</Markdown>
+    </div>
+  );
+}
+
+export default PostPage;
