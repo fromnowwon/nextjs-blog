7968e4ad1f5f30d328884fdd8bbb9ec9f70f69e8
cw <fromnowwon@gmail.com>
2023-02-15 14:36:19 +0900

    feat: 콘텐츠 객체 형식으로 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,38 @@
 import fs from 'fs';
 import Link from 'next/link';
+import matter from 'gray-matter';
+import { PostMetadata } from '@/components/PostMetadata';
 
-const getPostMetadata = () => { 
+const getPostMetadata = (): PostMetadata[] => { 
   const folder = "posts/";
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
-  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
-  return slugs;
+  
+  // Get gray-matter data from each file.
+  const posts = markdownPosts.map((fileName) => {
+    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf-8");
+    const matterResult = matter(fileContents);
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      subtitle: matterResult.data.subtitle,
+      slug: fileName.replace(".md", ""),
+    }
+  });
+
+  return posts;
 }
 
 const HomePage = () => {
   const postMetadata = getPostMetadata();
-  const postPreviews = postMetadata.map((slug, idx) => (
+  const postPreviews = postMetadata.map((post, idx) => (
     <div key={idx}>
-      <Link href={`/posts/${slug}`}>
-        <h2>{slug}</h2>
+      <Link href={`/posts/${post.slug}`}>
+        <h2>{post.title}</h2>
       </Link>
+      <p>{post.subtitle}</p>
+      <p>{post.date}</p>
+      <p></p>
     </div>
   ));
   return <div>{ postPreviews }</div>
