fec7b051c42c4a75d7b1dc05857b9666972c754f
cw <fromnowwon@gmail.com>
2023-02-17 13:57:01 +0900

    fix: tailwindcss config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
